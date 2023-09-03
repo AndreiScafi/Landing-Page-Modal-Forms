b12330ca2d957be30afc65e00a788284cffa43e5
Andrei Scafi <andreiscafi@hotmail.com>
2023-09-03 11:46:16 -0300

    Professor's javascript solution.

--- script.js
+++ script.js
@@ -9,21 +9,27 @@ const signupForm = document.querySelector('.signup-form-wrapper');
 const loginX = document.querySelector('.login-x');
 const signupX = document.querySelector('.signup-x');
 
+const formContainer = document.querySelector('.form-container');
+
 
 loginBtn.addEventListener('click', () => {
     loginForm.classList.add('active');
+    formContainer.classList.add('disable');
 });
 
 loginX.addEventListener('click', () => {
     loginForm.classList.remove('active');
+    formContainer.classList.remove('disable');
 });
 
 signupBtn.addEventListener('click', () => {
     signupForm.classList.add('active');
+    formContainer.classList.add('disable');
 });
 
 signupX.addEventListener('click', () => {
     signupForm.classList.remove('active');
+    formContainer.classList.remove('disable');
 });
 
 /* End of My own solution for javascript code */
